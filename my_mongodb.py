9e28861228516d45194f0c8beaccbe56d55698d9
katlegoJKS <katlego.malatjie@umuzi.org>
2020-04-24 14:49:58 +0200

    created 'created_visitor' script

--- my_mongodb.py
+++ my_mongodb.py
@@ -1,5 +1,20 @@
-from pymongo import MongoClient
+import pymongo
 
-client = MongoClient('mongodb://root:rootpass@localhost:27017')
+client = pymongo.MongoClient("mongodb://root:rootpass@localhost:27018/")
+db = client["UmuziProspects"]
+Visiter = db["Visiter"] 
 
-db = client.pymongo_UmuziProspects
+def create_visitor(visitor):
+    mydict = {
+        'visiter_name': 'Jack',
+        'visiters_age': '20',
+        'date_of_vist': '01 January 2020',
+        'time_of_visit': '13H00',
+        'name_of_the_person_who_assisted_visiter': 'Daniels',
+        'comments': 'Daniels was very much helpful'
+    }
+    result = Visiter.insert_one(mydict)
+    return result
+
+visitor = create_visitor('')
+print('One post: {0}'.format(visitor.inserted_id))
