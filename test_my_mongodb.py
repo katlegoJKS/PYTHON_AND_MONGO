6ddd11f30538fd38c5ce318c128163bc130bf4f5
katlegoJKS <katlego.malatjie@umuzi.org>
2020-04-28 16:24:58 +0200

    successfully tested all six functions using unittest

--- test_my_mongodb.py
+++ test_my_mongodb.py
@@ -1,8 +1,28 @@
 import unittest
 
 from my_mongodb import create_visitor
+from my_mongodb import list_visitors
+from my_mongodb import delete_visitor
+from my_mongodb import update_visitor
+from my_mongodb import visitor_details
+from my_mongodb import delete_all
 
 class TestMyMongdb(unittest.TestCase):
 
     def test_create_visitor(self):
         assert create_visitor
+
+    def test_list_visitors(self):
+        assert list_visitors
+
+    def test_delete_visitor(self):
+        assert delete_visitor
+
+    def test_update_visitor(self):
+        assert update_visitor
+
+    def test_visitor_details(self):
+        assert visitor_details
+
+    def test_delete_all(self):
+        assert delete_all
